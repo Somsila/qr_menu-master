e2f86f25c859218ae6eb14dd86c7138334efac73
@Somsila <somsila99@gmail.com>
2024-11-22 11:17:03 +0700

    adding dist

---
+++ dist/assets/AdminLayout-G-4lsXJT.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./AdminLayout.vue_vue_type_script_setup_true_lang-CZvS4z8_.js";import"./index-w1II4jnT.js";import"./DrinkProduct.vue_vue_type_script_setup_true_lang-iLHmFn6Y.js";export{o as default};
