225dcad70b50cb701a79f662d8b55af205c7b0bc
@Somsila <somsila99@gmail.com>
2024-11-22 11:17:03 +0700

    adding dist

---
+++ assets/DrinkProduct-BTge3U_x.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./DrinkProduct.vue_vue_type_script_setup_true_lang-iLHmFn6Y.js";import"./index-w1II4jnT.js";export{m as default};
---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/qr_menu-master/sw.js', { scope: '/qr_menu-master/' })})}
